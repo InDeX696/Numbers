0b8dd0082ac4f7edec68fd77c9800f45014f8488
Jonay <indexgamer696@gmail.com>
2019-09-17 09:56:33 +0100

    First Ten

--- src/main/java/Numbers.java
+++ src/main/java/Numbers.java
@@ -1,5 +1,38 @@
 public class Numbers {
     public static String get(int i) {
-        return i == 1 ? "one" : "cero";
+        String result="cero";
+        switch (i){
+            case 1:
+                result = "one";
+                break;
+            case 2:
+                result = "two";
+                break;
+            case 3:
+                result = "three";
+                break;
+            case 4:
+                result = "four";
+                break;
+            case 5:
+                result = "five";
+                break;
+            case 6:
+                result = "six";
+                break;
+            case 7:
+                result = "seven";
+                break;
+            case 8:
+                result = "eight";
+                break;
+            case 9:
+                result = "nine";
+                break;
+            case 10:
+                result = "ten";
+                break;
+        }
+        return result;
     }
 }
--- src/test/java/Numbers_.java
+++ src/test/java/Numbers_.java
@@ -14,6 +14,52 @@ public class Numbers_ {
 
         assertThat(Numbers.get(1)).isEqualTo("one");
     }
+    @Test
+    public void give_2_return_two(){
+
+        assertThat(Numbers.get(2)).isEqualTo("two");
+    }
+    @Test
+    public void give_3_return_three(){
+
+        assertThat(Numbers.get(3)).isEqualTo("three");
+    }
+    @Test
+    public void give_4_return_four(){
+
+        assertThat(Numbers.get(4)).isEqualTo("four");
+    }
+    @Test
+    public void give_5_return_five(){
+
+        assertThat(Numbers.get(5)).isEqualTo("five");
+    }
+    @Test
+    public void give_6_return_six(){
+
+        assertThat(Numbers.get(6)).isEqualTo("six");
+    }
+    @Test
+    public void give_7_return_seven(){
+
+        assertThat(Numbers.get(7)).isEqualTo("seven");
+    }
+    @Test
+    public void give_8_return_eight(){
+
+        assertThat(Numbers.get(8)).isEqualTo("eight");
+    }
+    @Test
+    public void give_9_return_nine(){
+
+        assertThat(Numbers.get(9)).isEqualTo("nine");
+    }
+    @Test
+    public void give_10_return_ten(){
+
+        assertThat(Numbers.get(10)).isEqualTo("ten");
+    }
+
 
 
 
