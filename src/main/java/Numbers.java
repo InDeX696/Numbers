c0fb083afff368ad0203ef8e87ed2ab6562420e0
Jonay <indexgamer696@gmail.com>
2019-09-18 17:37:56 +0100

    Units and tens

--- src/main/java/Numbers.java
+++ src/main/java/Numbers.java
@@ -1,38 +1,44 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Numbers {
     public static String get(int i) {
         String result="cero";
-        switch (i){
-            case 1:
-                result = "one";
-                break;
-            case 2:
-                result = "two";
-                break;
-            case 3:
-                result = "three";
-                break;
-            case 4:
-                result = "four";
-                break;
-            case 5:
-                result = "five";
-                break;
-            case 6:
-                result = "six";
-                break;
-            case 7:
-                result = "seven";
-                break;
-            case 8:
-                result = "eight";
-                break;
-            case 9:
-                result = "nine";
-                break;
-            case 10:
-                result = "ten";
-                break;
+        String number = i + "";
+
+        int  num = number.length();
+        if(num == 1) {
+            Map<Integer, String> units = initMapUnits();
+            return simple(units,i);
         }
-        return result;
+        if(num == 2) {
+           Map<Integer, String> tens = initMapTens();
+           return simple(tens, i);
+        }
+        return "Error";
+    }
+
+    private static Map<Integer, String> initMapTens() {
+        Map<Integer,String> tens = new HashMap<Integer, String>();
+        String L[] = new String[]{"ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
+        for(int i = 1; i<10; i++ ){
+            tens.put(i*10 , L[i-1]);
+        }
+        return tens;
+    }
+
+    public static String  simple(Map<Integer, String> map, int num){
+        if(map.containsKey(num)){
+            return map.get(num);
+        }
+        return "Error";
+    }
+    private static Map<Integer, String> initMapUnits(){
+        Map< Integer, String> units = new HashMap<Integer,String >();
+        String L[] = new String[]{"cero","one", "two","three","four","five","six","seven","eight","nine"};
+        for(int i = 0; i<10; i++ ){
+            units.put(i , L[i]);
+        }
+        return units;
     }
 }
